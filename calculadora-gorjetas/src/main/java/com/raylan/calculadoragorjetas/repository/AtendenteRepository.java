2efa90ed4433a922ed18839d69871afad76ac59e
DevRaylan <diegoraylan16@gmail.com>
2026-08-16 18:07:54 -0300

    Remove calculadora-gorjetas (movido para o repositório portfolio-projects)

--- calculadora-gorjetas/src/main/java/com/raylan/calculadoragorjetas/repository/AtendenteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.raylan.calculadoragorjetas.repository;
-
-import com.raylan.calculadoragorjetas.model.Atendente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AtendenteRepository extends JpaRepository<Atendente, Long> {
-}
